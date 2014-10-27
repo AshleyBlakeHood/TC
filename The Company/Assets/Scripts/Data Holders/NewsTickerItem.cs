b52128dbd2aec307766ad36631cf8e67859836f5
Jordan Bird <jordanbird-29@hotmail.co.uk>
2014-10-27 20:34:21 +0000

    News Ticker

---
+++ The Company/Assets/Scripts/Data Holders/NewsTickerItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewsTickerItem
+{
+	public string headline = "Unitialised Headline";
+	public int timesToShow = 1;
+	public bool repeat = false;
+
+	public NewsTickerItem(string iHeadline, bool iRepeat)
+	{
+		headline = iHeadline;
+		repeat = iRepeat;
+	}
+}
---
+++ The Company/Assets/Scripts/NewsTicker.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewsTicker : MonoBehaviour
+{
+	private List<NewsTickerItem> newsItems = new List<NewsTickerItem>();
+	
+	public GameObject newstickerCanvas;
+	private Text ticker;
+
+	public float speed = 30;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ticker = newstickerCanvas.GetComponentInChildren<Text> ();
+
+		newsItems.Add (new NewsTickerItem ("1/2 Life III Project Cancelled", true));
+		newsItems.Add (new NewsTickerItem ("News Headline 2", true));
+		newsItems.Add (new NewsTickerItem ("Everything is Awesome", true));
+
+		ticker.text = newsItems[0].headline;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		ticker.transform.position = new Vector2 (ticker.transform.position.x - (speed * Time.deltaTime), ticker.transform.position.y);
+
+		if (ticker.rectTransform.position.x * 2 < -ticker.rectTransform.rect.width)
+		{
+			//Update Item
+			ticker.rectTransform.position = new Vector2 (ticker.rectTransform.rect.width * 1.5f, ticker.transform.position.y);
+
+			int item = Random.Range (0, newsItems.Count);
+			ticker.text = newsItems[item].headline;
+
+			if (!newsItems[item].repeat)
+				newsItems.RemoveAt (newsItems[item]);
+		}
+	}
+
+
+}
