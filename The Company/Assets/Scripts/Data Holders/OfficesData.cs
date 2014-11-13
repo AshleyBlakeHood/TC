10b9ea622747ff9986bd5ce4ff1af5ce7dda6cd4
Syraneth <peterfkeen@hotmail.com>
2014-11-13 18:34:37 +0000

    Offices

---
+++ The Company/Assets/Scripts/Data Holders/OfficesData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadquartersData : MonoBehaviour {
+	//public SafehouseData (int fId, int iAgent, int iEquipment, int iVehicles)
+	//Training, Agent, Storage, Wages, Recruitment, Stat tracking, Coffee drank, Printer, waste, Agents lost
+
+	private int waste, coffeeCups, lostAgents, printerInk, deskPops, matliniShaken;
+}
