407e2b15791d0c64f7fb81613d046d95193ad07d
Peter Keen <peterfkeen@hotmail.com>
2015-01-28 14:32:28 +0000

    COMMENTS!

--- The Company/Assets/Scripts/Continent.cs
+++ The Company/Assets/Scripts/Continent.cs
@@ -2,30 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+/// <summary>
+/// The Continent class holds the headquarters located on the continent
+/// </summary>
 public class Continent : MonoBehaviour
 {
+	//Attributes
 	public List<int> hqList = new List<int>();
-
-	public Sprite fineSprite;
+	//The areaID gives the continent an id. This is used in retrieving the correct continent
 	public string areaID;
 
-	void Start () {
-		fineSprite = GetComponent<SpriteRenderer>().sprite;
-	}
-
-	void Update()
-	{
-
-	}
-
+	/// <summary>
+	/// Adds the ID of the headquarters to the continent class's list of headquarters. This is useful in tracking where the headquarters are located
+	/// </summary>
+	/// <param name="hq">Hq.</param>
 	public void addAreaHQ(int hq)
 	{
 		hqList.Add (hq);
 	}
-
-	public void loadDeadzone()
-	{
-
-	}
-
 }
